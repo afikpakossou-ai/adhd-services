349b8add36ee7ef246359bb707332667bf3793fd
afikpakossou-ai <afikpakossou@gmail.com>
2025-09-27 04:36:47 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,3 +10,7 @@ export default defineConfig({
     outDir: 'dist'
   }
 })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+export default defineConfig({ plugins: [react()], base: '/', build: { outDir: 'dist' } })
+
